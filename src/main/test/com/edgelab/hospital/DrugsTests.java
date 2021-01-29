935661dc9516c81c514b3a27641cfd9d22f83958
Geoffrey Vincent <geoffrey.vincent@gamaya.com>
2021-01-29 11:06:19 +0100

    better tests

--- src/main/test/com/edgelab/hospital/DrugsTests.java
+++ src/main/test/com/edgelab/hospital/DrugsTests.java
@@ -1,7 +1,6 @@
 package com.edgelab.hospital;
 
 import com.edgelab.hospital.entities.Drug;
-import com.edgelab.hospital.entities.State;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -10,38 +9,7 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-public class DrugsAndStatesTests {
-
-    @Test
-    void emptyState() {
-        assertEquals(new ArrayList<>(), State.parseStates(""));
-    }
-
-    @Test
-    void parseFever() {
-        assertEquals(State.FEVER, State.parseStates("F").get(0));
-    }
-
-    @Test
-    void parseHealthy() {
-        assertEquals(State.HEALTHY, State.parseStates("H").get(0));
-    }
-
-    @Test
-    void parseDiabetes() {
-        assertEquals(State.DIABETES, State.parseStates("D").get(0));
-    }
-
-    @Test
-    void parseTuberculosis() {
-        assertEquals(State.TUBERCULOSIS, State.parseStates("T").get(0));
-    }
-
-    @Test
-    void parseMultipleStates() {
-        assertEquals(Arrays.asList(State.FEVER, State.HEALTHY, State.DIABETES, State.TUBERCULOSIS, State.DEAD), State.parseStates("F,H,D,T,X"));
-        assertNotEquals(Arrays.asList(State.FEVER, State.HEALTHY, State.DIABETES, State.TUBERCULOSIS, State.DEAD), State.parseStates("H,F,D,T,X"));
-    }
+public class DrugsTests {
 
     @Test
     void emptyDrug() {
